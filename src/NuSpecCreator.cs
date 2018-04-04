345c4805cf9b9a7a8f6cda9f47465d3d6cf03b18
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-04-04 11:07:19 +0200

    Unwanted exe file in nuget package

--- src/NuSpecCreator.cs
+++ src/NuSpecCreator.cs
@@ -135,7 +135,7 @@ namespace Aspenlaub.Net.GitHub.CSharp.Shatilaya {
             foreach (var fileElement in new[] { @"dll", @"pdb" }.Select(extension
                   => new XElement(Namespace + @"file",
                       new XAttribute(@"src", outputPathElement.Value + topLevelNamespace + ".*." + extension),
-                      new XAttribute(@"exclude", outputPathElement.Value + @"*.Test*.*"),
+                      new XAttribute(@"exclude", string.Join(",", outputPathElement.Value + @"*.Test*.*", outputPathElement.Value + @"*.exe")),
                       new XAttribute(@"target", @"lib\net" + targetFrameworkElement.Value.Replace("v", "").Replace(".", ""))))) {
                 filesElement.Add(fileElement);
             }
