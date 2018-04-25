e2a89215a235970b79379e263d90629e9bbf8881
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-04-25 22:34:40 +0200

    Cake 0.27 was a catastrophe, so from now on do not always use the latest release

--- src/Test/CakeInstaller.cs
+++ src/Test/CakeInstaller.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Aspenlaub.Net.GitHub.CSharp.Pegh;
 using LibGit2Sharp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
@@ -9,15 +13,26 @@ using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Shatilaya.Test {
     public class CakeInstaller {
+        protected const string CakeVersion = "0.26.0";
+
         public void InstallCake(IFolder cakeFolder) {
             var gitUtilities = new GitUtilities();
             var errorsAndInfos = new ErrorsAndInfos();
             const string url = "https://github.com/cake-build/example";
-            gitUtilities.Clone(url, cakeFolder, new CloneOptions { BranchName = "master" }, true, () => File.Exists(CakeExeFileFullName(cakeFolder)), () => RunBuildPs1(cakeFolder), errorsAndInfos);
+            gitUtilities.Clone(url, cakeFolder, new CloneOptions { BranchName = "master" }, true, () => File.Exists(CakeExeFileFullName(cakeFolder)), () => FixCakeVersionAndRunBuildPs1(cakeFolder), errorsAndInfos);
             Assert.IsFalse(errorsAndInfos.Errors.Any(), string.Join("\r\n", errorsAndInfos.Errors));
         }
 
-        private void RunBuildPs1(IFolder cakeFolder) {
+        private void FixCakeVersionAndRunBuildPs1(IFolder cakeFolder) {
+            var packagesConfigFileFullName = cakeFolder.SubFolder("tools").FullName + @"\packages.config";
+            var document = XDocument.Load(packagesConfigFileFullName);
+            var element = document.XPathSelectElements("/packages/package").FirstOrDefault(e => e.Attribute("id")?.Value == "Cake");
+            Assert.IsNotNull(element);
+            var attribute = element.Attribute("version");
+            Assert.IsNotNull(attribute);
+            attribute.SetValue(CakeVersion);
+            document.Save(packagesConfigFileFullName);
+
             var powershellExecuter = new PowershellExecuter();
             IList<string> errors;
             powershellExecuter.ExecutePowershellScriptFile(cakeFolder.FullName + @"\build.ps1", out errors);
