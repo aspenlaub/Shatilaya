934e538ca31a135c191f3c03fe9f965ff3a9a89c
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-10-13 08:55:20 +0200

    Do not add anything if there were no errors + avoid duplicates

--- src/Extensions/ErrorsAndInfosExtension.cs
+++ src/Extensions/ErrorsAndInfosExtension.cs
@@ -5,9 +5,11 @@ using Aspenlaub.Net.GitHub.CSharp.PeghStandard.Interfaces;
 namespace Aspenlaub.Net.GitHub.CSharp.Shatilaya.Extensions {
     public static class ErrorsAndInfosExtension {
         public static string ErrorsPlusRelevantInfos(this IErrorsAndInfos errorsAndInfos) {
+            if (!errorsAndInfos.AnyErrors()) { return ""; }
+
             var components = new List<string>();
             components.AddRange(errorsAndInfos.Errors);
-            components.AddRange(errorsAndInfos.Infos.Where(i => i.StartsWith("Failed") || i.Contains(".trx")));
+            components.AddRange(errorsAndInfos.Infos.Where(i => (i.StartsWith("Failed") || i.Contains(".trx")) && !components.Contains(i)));
             return string.Join("\r\n", components);
         }
     }
