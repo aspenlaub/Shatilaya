d76b5ed130292575dce56e241d6f73966285361d
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-02-09 11:19:29 +0100

    Nuclide update

---
+++ src/Test/NuclideVersionCheck.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Reflection;
+using Aspenlaub.Net.GitHub.CSharp.Gitty;
+using Aspenlaub.Net.GitHub.CSharp.Gitty.Interfaces;
+using Aspenlaub.Net.GitHub.CSharp.Gitty.TestUtilities;
+using Aspenlaub.Net.GitHub.CSharp.Nuclide;
+using Aspenlaub.Net.GitHub.CSharp.Nuclide.Interfaces;
+using IContainer = Autofac.IContainer;
+using Autofac;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Shatilaya.Test {
+    [TestClass]
+    public class NuclideVersionCheck {
+        private static IContainer vContainer;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context) {
+            vContainer = new ContainerBuilder().UseGittyTestUtilities().UseNuclideProtchAndGitty().Build();
+        }
+
+        [TestMethod]
+        public void BuildCakeUsesRightPackageVersion() {
+            var version = typeof(INugetPackageToPushFinder).Assembly.GetName().Version;
+            Assert.IsTrue(version.ToString().StartsWith("1.0."));
+            var buildCake = vContainer.Resolve<IEmbeddedCakeScriptReader>().ReadCakeScriptFromAssembly(Assembly.GetExecutingAssembly(), BuildCake.Standard).Split("\n");
+            Assert.IsTrue(buildCake.Any(s => s.Contains("Nuclide") & s.Contains($"version={version.ToString()}")),
+                $"Build cake does not use Nuclide version {version}");
+        }
+    }
+}
