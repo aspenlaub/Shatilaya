e21d6b14a9d28c49ea68875f55850174a9ac6cb6
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-08 17:03:46 +0100

    Implement missing tasks

---
+++ src/CakeFrosting/Tasks/DoNotPushTask.cs
@@ -0,0 +1,21 @@
+﻿using Cake.Common.Diagnostics;
+using Cake.Frosting;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Shatilaya.CakeFrosting.Tasks;
+
+[TaskName("DoNotPush")]
+[TaskDescription("Default except check nuget push")]
+[IsDependentOn(typeof(CleanRestorePullTask))]
+[IsDependentOn(typeof(VerifyThatThereAreNoUncommittedChangesTask))]
+[IsDependentOn(typeof(VerifyThatDevelopmentBranchIsAheadOfMasterTask))]
+[IsDependentOn(typeof(VerifyThatMasterBranchDoesNotHaveOpenPullRequestsTask))]
+[IsDependentOn(typeof(VerifyThatDevelopmentBranchDoesNotHaveOpenPullRequestsTask))]
+[IsDependentOn(typeof(VerifyThatPullRequestExistsForDevelopmentBranchHeadTipTask))]
+[IsDependentOn(typeof(BuildAndTestDebugAndReleaseTask))]
+[IsDependentOn(typeof(UpdateNuspecTask))]
+[IsDependentOn(typeof(CreateNuGetPackageTask))]
+public class DoNotPushTask : FrostingTask<ShatilayaContext> {
+    public override void Run(ShatilayaContext context) {
+        context.Information("Default except check nuget push");
+    }
+}
