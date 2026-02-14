5da17878a979ce8f565cadc785685b821e852c24
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-14 16:08:51 +0100

    solution.cake files are being removed

--- solution.cake
+++
@@ -1,6 +0,0 @@
-var solution = "./src/Shatilaya.slnx";
-var masterDebugBinFolder = "../ShatilayaBin/Debug";
-var masterReleaseBinFolder = "../ShatilayaBin/Release";
-var solutionSpecialSettingsDictionary = new Dictionary<string, string> {
-    { "CreateAndPushPackages",  "false" }
-};
--- src/Components/SolutionCakeConverter.cs
+++ src/Components/SolutionCakeConverter.cs
@@ -13,41 +13,42 @@ public class SolutionCakeConverter {
 
     public static async Task ConvertSolutionCakeAsync(IFolder repositoryFolder, IErrorsAndInfos errorsAndInfos) {
         string solutionCakeFullFileName = repositoryFolder.FullName + @"\\solution.cake";
+        if (!File.Exists(solutionCakeFullFileName)) {
+            return;
+        }
+
         string solutionJsonFileName = solutionCakeFullFileName.Replace("solution.cake", "solution.json");
         if (solutionCakeFullFileName == solutionJsonFileName) {
             errorsAndInfos.Errors.Add("Solution cake and js file names are identical");
             return;
         }
 
-        string contents = "{}";
-        if (File.Exists(solutionCakeFullFileName)) {
-            contents = await File.ReadAllTextAsync(solutionCakeFullFileName);
-            int pos = contents.IndexOf(_startTag, StringComparison.OrdinalIgnoreCase);
-            if (pos >= 0) {
-                int pos2 = contents.IndexOf("};", pos + 1, StringComparison.OrdinalIgnoreCase);
-                if (pos2 < 0) {
-                    errorsAndInfos.Errors.Add("Could not find dictionary in solution.cake");
-                    return;
-                }
-
-                contents = contents.Substring(pos + _startTag.Length - 1, pos2 - pos - _startTag.Length + 2).Trim();
-                contents = contents.Replace("{ \"", "\"");
-                contents = contents.Replace("\", \"", "\": \"");
-                contents = contents.Replace("\",  \"", "\": \"");
-                contents = contents.Replace("\" }", "\",");
-                pos = contents.LastIndexOf("\",", StringComparison.OrdinalIgnoreCase);
-                if (pos < 0) {
-                    errorsAndInfos.Errors.Add("Could not find dictionary in solution.cake");
-                    return;
-                }
-
-                contents = contents.Substring(0, pos + 1) + contents.Substring(pos + 2);
-            } else if (contents.Contains(_noTag, StringComparison.OrdinalIgnoreCase)) {
-                contents = "{}";
-            } else {
+        string contents = await File.ReadAllTextAsync(solutionCakeFullFileName);
+        int pos = contents.IndexOf(_startTag, StringComparison.OrdinalIgnoreCase);
+        if (pos >= 0) {
+            int pos2 = contents.IndexOf("};", pos + 1, StringComparison.OrdinalIgnoreCase);
+            if (pos2 < 0) {
                 errorsAndInfos.Errors.Add("Could not find dictionary in solution.cake");
                 return;
             }
+
+            contents = contents.Substring(pos + _startTag.Length - 1, pos2 - pos - _startTag.Length + 2).Trim();
+            contents = contents.Replace("{ \"", "\"");
+            contents = contents.Replace("\", \"", "\": \"");
+            contents = contents.Replace("\",  \"", "\": \"");
+            contents = contents.Replace("\" }", "\",");
+            pos = contents.LastIndexOf("\",", StringComparison.OrdinalIgnoreCase);
+            if (pos < 0) {
+                errorsAndInfos.Errors.Add("Could not find dictionary in solution.cake");
+                return;
+            }
+
+            contents = contents.Substring(0, pos + 1) + contents.Substring(pos + 2);
+        } else if (contents.Contains(_noTag, StringComparison.OrdinalIgnoreCase)) {
+            contents = "{}";
+        } else {
+            errorsAndInfos.Errors.Add("Could not find dictionary in solution.cake");
+            return;
         }
 
         if (File.Exists(solutionJsonFileName) && await File.ReadAllTextAsync(solutionJsonFileName) == contents) {
