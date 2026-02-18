4a9c1f5c4e75c164df52900bc297b9f412cef599
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-18 10:15:00 +0100

    Give some hints as to why no package was created

--- src/Tasks/CreateNuGetPackageTask.cs
+++ src/Tasks/CreateNuGetPackageTask.cs
@@ -1,10 +1,12 @@
 using System;
+using System.IO;
 using System.Linq;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Entities;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Extensions;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.Protch.Interfaces;
 using Autofac;
+using Cake.Common.Diagnostics;
 using Cake.Common.Tools.DotNet;
 using Cake.Common.Tools.DotNet.Pack;
 using Cake.Frosting;
@@ -19,6 +21,7 @@ public class CreateNuGetPackageTask : FrostingTask<ShatilayaContext> {
     }
 
     public override void Run(ShatilayaContext context) {
+        context.Information("Creating nuget package in the master Release binaries folder");
         var errorsAndInfos = new ErrorsAndInfos();
         IProjectFactory projectFactory = context.Container.Resolve<IProjectFactory>();
         IProjectLogic projectLogic = context.Container.Resolve<IProjectLogic>();
@@ -35,16 +38,40 @@ public class CreateNuGetPackageTask : FrostingTask<ShatilayaContext> {
             throw new Exception(errorsAndInfos.ErrorsToString());
         }
 
+        context.Information("All configurations have nuspec files");
         IFolder folder = context.MasterBinReleaseFolder;
-        if (!folder.LastWrittenFileFullName().EndsWith("nupkg")) {
-            var settings = new DotNetPackSettings {
-                Configuration = "Release",
-                NoBuild = true,
-                NoRestore = true,
-                IncludeSymbols = false,
-                OutputDirectory = context.MasterBinReleaseFolder.FullName
-            };
-            context.DotNetPack(context.RepositoryFolder.FullName + @"\src\" + context.SolutionId + ".csproj", settings);
+        if (folder.LastWrittenFileFullName().EndsWith("nupkg")) {
+            context.Information("Last written file name ends with nupkg");
+            return;
         }
+
+        string csProjFileFullName = context.RepositoryFolder.FullName + @"\src\" + context.SolutionId + ".csproj";
+        var settings = new DotNetPackSettings {
+            Configuration = "Release",
+            NoBuild = true,
+            NoRestore = true,
+            IncludeSymbols = false,
+            OutputDirectory = context.MasterBinReleaseFolder.FullName,
+            Verbosity = DotNetVerbosity.Diagnostic
+        };
+        context.Information($"Creating package for {csProjFileFullName}");
+        context.Information($"Creating package in {settings.OutputDirectory.FullPath.Replace('/', '\\')}");
+        context.DotNetPack(csProjFileFullName, settings);
+
+        string lastWrittenFileName = folder.LastWrittenFileFullName();
+        if (lastWrittenFileName.EndsWith("nupkg")) {
+            context.Information($"Package {lastWrittenFileName} was created");
+            return;
+        }
+
+        if (!File.Exists(settings.OutputDirectory.FullPath + "/packageicon.png")) {
+            throw new Exception($"No package was created. packageicon.png not found in {settings.OutputDirectory.FullPath.Replace('/', '\\')}");
+        }
+
+        if (!File.ReadAllText(csProjFileFullName).Contains("IsPackable")) {
+            throw new Exception($"No package was created. Add <IsPackable>true</IsPackable> to {csProjFileFullName}");
+        }
+
+        throw new Exception("Last written file name should end with nupkg");
     }
 }
